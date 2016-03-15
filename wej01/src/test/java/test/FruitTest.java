a4d9946248c3710edc9da8b16fd8252a3b20a81f
amilancej <a.milan@interia.pl>
2016-03-15 09:25:07 +0100

    wejsciowka

---
+++ wej01/src/test/java/test/FruitTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+
+import fruit.Fruit;
+
+public class FruitTest {
+	Fruit apple = new Fruit("Apple", "Spain");
+	Fruit pear = new Fruit("Pear", "Poland");
+	
+	public List<Fruit> fruits = new ArrayList<>();
+	
+	@Test
+	public void addTest() throws Exception{
+		assertEquals(fruits.size(), 0);
+		fruits.add(1, apple);
+		assertEquals(fruits.size(), 1);
+		fruits.add(2, pear);
+		assertEquals(fruits.size(), 2);
+	}
+}
