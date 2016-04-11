4843bf061d52ccd1ab1562c9a02d36e8d3e61698
Artur Milancej <Artur Milancej>
2016-04-11 18:04:37 +0200

    Poprawka jbehave

--- lab05/messenger 2/src/test/java/com/example/mockdemo/app/MessengerSteps.java
+++ lab05/messenger 2/src/test/java/com/example/mockdemo/app/MessengerSteps.java
@@ -8,14 +8,10 @@ import static org.junit.Assert.assertThat;
 import org.jbehave.core.annotations.Given;
 import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
-import org.junit.Before;
-import org.junit.Test;
 
-import com.example.mockdemo.messenger.ConnectionStatus;
 import com.example.mockdemo.messenger.MalformedRecipientException;
 import com.example.mockdemo.messenger.MessageService;
 import com.example.mockdemo.messenger.MessageServiceSimpleImpl;
-import com.example.mockdemo.messenger.SendingStatus;
 
 public class MessengerSteps {
 	private static final String VALID_SERVER = "inf.ug.edu.pl";
@@ -27,44 +23,47 @@ public class MessengerSteps {
 	private Messenger messenger;
 	private MessageService ms;
 	
+	private static String server;
+	private static String message;
+	
 	@Given("a messenger")
 	public void messengerSetup() throws MalformedRecipientException{
 		ms = new MessageServiceSimpleImpl();
-		messenger = new Messenger(ms);
-		
+		messenger = new Messenger(ms);		
 	}
 	
-	@When("server connection succeed")
-	public void checkValidConnection(){
-		assertEquals(0, messenger.testConnection(VALID_SERVER));
+	@When("connecting with VALID_SERVER")
+	public void setValidServer(){
+		server = VALID_SERVER;
 	}
 	
-	@Then("sending valid message should return $result")
-	public void sendingValidMessage(int result){
-		assertThat(messenger.sendMessage(VALID_SERVER, VALID_MESSAGE),
-				either(equalTo(0)).or(equalTo(1)));
+	@When("connecting with INVALID_SERVER")
+	public void setInvalidServer(){
+		server = INVALID_SERVER;
 	}
 	
-	@Then("sending invalid message should return $result")
-	public void sendingInvalidMessage(int result){
-		assertThat(messenger.sendMessage(VALID_SERVER, INVALID_MESSAGE),
-				either(equalTo(2)).or(equalTo(-1)));
+	@When("sending valid message")
+	public void setValidMessage(){
+		message = VALID_MESSAGE;
 	}
 	
-	@When("server connection failed")
-	public void checkInvalidConnection(){
-		assertEquals(1, messenger.testConnection(INVALID_SERVER));
+	@When("sending invalid message")
+	public void setInvalidMessage(){
+		message = INVALID_MESSAGE;
 	}
 	
-	@Then("sending valid message should return 1")
-	public void checkValidMessage1(){
-		assertThat(messenger.sendMessage(INVALID_SERVER, INVALID_MESSAGE),
-				either(equalTo(2)).or(equalTo(-1)));
+	@Then("message status on VALID_SERVER should return $result1 or $result2")
+	public void testValMsgValSrv(int result1, int result2){
+		assertThat(messenger.sendMessage(VALID_SERVER, message), either(equalTo(result1)).or(equalTo(result2)));
 	}
 	
-	@Then("sending invalid message should return 1")
-	public void checkInvalidMessage1(){
-		assertThat(messenger.sendMessage(INVALID_SERVER, INVALID_MESSAGE),
-				either(equalTo(2)).or(equalTo(-1)));
-	}
+	@Then("connection status should return $result")
+	    public void checkServer(int result){
+	        assertEquals(result, messenger.testConnection(server));
+	    }
+	
+	@Then("message status on INVALID_SERVER should return $result")
+	public void testValMsgInvalSrv(int result){
+		assertEquals(result, messenger.sendMessage(INVALID_SERVER, message));
+	}	
 }
--- lab05/messenger 2/src/test/resources/com/example/mockdemo/app/messenger_test.story
+++ lab05/messenger 2/src/test/resources/com/example/mockdemo/app/messenger_test.story
@@ -1,13 +1,16 @@
 Given a messenger
 
-When server connection succeed
-Then sending valid message should return 0
+When connecting with VALID_SERVER
+Then connection status should return 0
 
-When server connection succeed
-Then sending invalid message should return 1
+When connecting with INVALID_SERVER
+Then connection status should return 1
 
-When server connection failed
-Then sending valid message should return 1
+When sending valid message
+Then message status on VALID_SERVER should return 1 or 0
 
-When server connection failed
-Then sending invalid message should return 1
+When sending valid message
+Then message status on INVALID_SERVER should return 1
+
+When sending invalid message
+Then message status on INVALID_SERVER should return 2
