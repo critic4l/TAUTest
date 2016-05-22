97781574bcdd98087fe099b0b7b934f27330332d
critic4l <a.milancej@gmail.com>
2016-05-22 16:41:09 +0200

    CarManager + CarREST

---
+++ lab09/restservicedemo/scripts/klient.bat
@@ -0,0 +1,1 @@
+java -cp .\hsqldb-2.3.3.jar org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:hsql://localhost/workdb
--- lab09/restservicedemo/src/main/java/com/example/restservicedemo/rest/CarRESTService.java
+++ lab09/restservicedemo/src/main/java/com/example/restservicedemo/rest/CarRESTService.java
@@ -1,6 +1,7 @@
 package com.example.restservicedemo.rest;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,24 +11,33 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.example.restservicedemo.domain.Car;
+import com.example.restservicedemo.service.CarManager;
 
 @Path("car")
-public class CarFakeRESTService {	
+public class CarRESTService {	
+	
+	private CarManager cm = new CarManager();
 	
 	@GET
 	@Path("/{carId}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Car getCar(@PathParam("carId") Long id){
-		return new Car(1L, "Opel", "Corsa", 2005);
+		Car c = cm.getCar(id);
+		return c;
 	}
 	
 	@POST
 	@Path("/")
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response createTrackInJSON(Car car) {
- 
-		String result = "Car saved: " + car;
-		return Response.status(201).entity(result).build(); 
+		cm.addCar(car);
+		return Response.status(201).entity("Car").build();
+	}
+	
+	@DELETE
+	public Response clearCars() {
+		cm.clearCars();
+		return Response.status(200).build();
 	}
 
 }
